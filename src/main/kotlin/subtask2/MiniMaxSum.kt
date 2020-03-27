9792f54f8ff1fe7b53b60c22cb4481ba643712a0
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2020-03-27 10:44:07 +0300

    Solve mini-max sum task

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -2,8 +2,16 @@ package subtask2
 
 class MiniMaxSum {
 
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val min = input.min()
+        val max = input.max()
+
+        if (min == null || max == null) {
+            throw Exception("array must be an array of positive integers")
+        }
+
+        val sum = input.sum()
+        return intArrayOf(sum - max, sum - min)
     }
 }
+
