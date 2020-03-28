4813f68a4ece4782db8460b081a2a21c59308e63
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2020-03-29 00:36:30 +0300

    Solve string parser task

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,36 @@
 package subtask4
 
+val startBrackets: HashMap<Char, Char> = hashMapOf(
+    '(' to ')',
+    '[' to ']',
+    '<' to '>'
+)
+
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = mutableListOf<String>()
+        val lastIndex = inputString.length - 1
+
+        inputString.slice(0 until lastIndex).forEachIndexed loop@{ startIndex, startBracket ->
+
+            if (startBracket in startBrackets) {
+                val endBracket = startBrackets[startBracket]
+                var count = 1
+
+                inputString.slice(startIndex + 1..lastIndex).forEachIndexed { endIndex, bracket ->
+                    when (bracket) {
+                        startBracket -> count += 1
+                        endBracket -> count -= 1
+                    }
+                    if (count == 0) {
+                        result.add(inputString.substring(startIndex + 1, startIndex + 1 + endIndex))
+                        return@loop
+                    }
+                }
+            }
+        }
+
+        return result.toTypedArray()
     }
 }
